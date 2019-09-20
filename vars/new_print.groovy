2c1bb07403775801f23f977e5a37c1b1eb7beb70
rvmuralimohan <mohanmurali.r@gmail.com>
2019-09-20 17:53:55 +0530

    new_print.groovy

---
+++ vars/new_print.groovy
@@ -0,0 +1,24 @@
+public class Demo3 {
+
+    public static void main(String[] args) {
+        String dateStr ="Nov 28 20:13:06 2019";
+        String dateStr2 = "Nov 30 20:13:06 2019";
+        SimpleDateFormat format = new SimpleDateFormat("MMM dd H:m:s yyyy");
+        SimpleDateFormat format2 = new SimpleDateFormat("MMM dd H:m:s yyyy");
+        try {
+            Date date2 = format.parse(dateStr2);
+            Date date = format.parse(dateStr);
+
+            System.out.println("distance is :"+differentDaysByMillisecond(date,date2));
+        }catch(ParseException e ){
+            e.printStackTrace();
+        }
+    }
+
+//get Days method
+
+    private static int differentDaysByMillisecond(Date date, Date date2) {
+        return (int)((date2.getTime()-date.getTime())/1000/60/60/24);
+    }
+
+}
