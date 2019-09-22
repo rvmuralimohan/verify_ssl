cb84ca2c29985ebfb5b81e9a611956ba6fe6c78f
rvmuralimohan <mohanmurali.r@gmail.com>
2019-09-22 08:44:47 +0530

    jira

--- vars/duration.groovy
+++ vars/duration.groovy
@@ -8,9 +8,9 @@ def endDate = expiryDateStr.format("MMM dd H:m:s yyyy")
 def cfm = ComponentAccessor.customFieldManager
 def issueManager = ComponentAccessor.issueManager
 
-#def startDate = cfm.getCustomFieldObjectByName("Start Date CF Name")
-#def endDate = cfm.getCustomFieldObjectByName("End Date CF Name")
-#def days = cfm.getCustomFieldObjectByName("Days")
+//def startDate = cfm.getCustomFieldObjectByName("Start Date CF Name")
+..#def endDate = cfm.getCustomFieldObjectByName("End Date CF Name")
+//#def days = cfm.getCustomFieldObjectByName("Days")
 
 def issueStartDate = issue.getCustomFieldValue(startDate) as Date
 def issueEndDate = issue.getCustomFieldValue(endDate) as Date
@@ -20,6 +20,6 @@ use(groovy.time.TimeCategory) {
     duration = (issueEndDate - issueStartDate).days
 }
 
-issue.setCustomFieldValue(days,duration)
-issueManager.updateIssue(ComponentAccessor.jiraAuthenticationContext.loggedInUser, issue, EventDispatchOption.DO_NOT_DISPATCH, false)
+//issue.setCustomFieldValue(days,duration)
+//issueManager.updateIssue(ComponentAccessor.jiraAuthenticationContext.loggedInUser, issue, EventDispatchOption.DO_NOT_DISPATCH, false)
 
