e3c8884618674722041e24806732df2dddec4980
rvmuralimohan <mohanmurali.r@gmail.com>
2019-12-30 10:37:53 +0530

    new featcher

---
+++ certi.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+expiredate =`openssl x509 -in /path/to/cert.pem -inform PEM -text -noout -enddate | grep "Not After" | awk '{print $4, $5, $7}'`
+TodayPlus30 =`date -ud "+30 day" | awk '{print $2, $3, $6}'`
+`
+
