b4a700e6a934ccc4fcd80cd12b18c1e06a391831
rvmuralimohan <mohanmurali.r@gmail.com>
2020-03-11 20:05:56 +0530

    updated certi.sh

--- certi.sh
+++ certi.sh
@@ -2,5 +2,5 @@
 
 expiredate =`openssl x509 -in /path/to/cert.pem -inform PEM -text -noout -enddate | grep "Not After" | awk '{print $4, $5, $7}'`
 TodayPlus30 =`date -ud "+30 day" | awk '{print $2, $3, $6}'`
-`
+
 
